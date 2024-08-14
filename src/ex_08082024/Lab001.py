de16773fdc77388723d3975197b014fc086c7af8
dheeraj <dheeraj122000@gmail.com>
2024-08-14 10:30:43 +0530

    Initial commit

---
+++ src/ex_08082024/Lab001.py
@@ -0,0 +1,6 @@
+# This is a comment - which does not run while execution.
+
+print("Hello Dheeraj, Welcome to the Python Learning.")
+print(5+5)
+print(5-5)
+print(10**0)
---
+++ src/ex_10082024/Lab003.py
@@ -0,0 +1,26 @@
+print("Hello World!")
+
+print("Hello", "Naruto", "Tanjiro", sep=' $$$ ')
+print("Hello", "Hyderabad", "Cricket", end='.')
+print("Good", "Dj")
+
+import keyword
+print(keyword.kwlist)
+
+print("Hello, " +"World!")
+# Hello, World!
+
+# Write a Python script that asks the user for their age and
+# then prints a message stating their age.
+'''
+age = int(input("Enter your age: "))
+print(f"Your age is: {age}")
+'''
+text_1 = 'Python'
+print(text_1[0:3])
+
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+
+area_of_rect = length * width
+print(f"The area of the rectangle is: {area_of_rect}")
